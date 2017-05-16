c7ac14523f85112eb861f275e6de2bb50fbc8656
dc-coder <mir.mir@gmail.com>
2017-05-16 16:33:55 -0400

    edits after team feedback

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-$('.show-dialog').click(function(){
+// $('.show-dialog').click(function(){
 
-  var doc = $(this).next().get(0); 
+//   var doc = $(this).next().get(0); 
 
-  var dialog =   $(this).next().get(0);
+//   var dialog =   $(this).next().get(0);
   
-  var showDialogButton = $(this).get(0);
+//   var showDialogButton = $(this).get(0);
    
-  if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-  }
-  showDialogButton.addEventListener('click', function() {
-    dialog.showModal();
-  });
-  dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-  });
+//   if (! dialog.showModal) {
+//     dialogPolyfill.registerDialog(dialog);
+//   }
+//   showDialogButton.addEventListener('click', function() {
+//     dialog.showModal();
+//   });
+//   dialog.querySelector('.close').addEventListener('click', function() {
+//     dialog.close();
+//   });
 
 
-});
+// });
 
 
- 
+//  
